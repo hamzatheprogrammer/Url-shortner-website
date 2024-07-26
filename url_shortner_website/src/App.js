601eb32bf3df52e75001bd153b6e1b16276ff331
Hamza saleem <rydergamer1122@gmail.com>
2024-07-26 12:53:20 +0500

    "Added Bootstrap dependency and imported CSS in App.js, updated package.json and package-lock.json accordingly"

--- url_shortner_website/src/App.js
+++ url_shortner_website/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const [url, setUrl] = React.useState(null)
   const [shortedUrl, setShortedurl] = React.useState("")
@@ -10,10 +10,19 @@ function App() {
     setShortedurl(response.data.url)
   }
   return (
-    <div>
-      <p>{shortedUrl}</p>
-      <input type="text" onChange={(e)=> setUrl(e.target.value)}/>
-      <button onClick={fetchUrl}>Short Url</button>
+    <div className='container'>
+      <h1 className='text-center mt-3 mb-3'>Url Shortner</h1>
+      <div className='input-group mb-3 mt-3'>
+      <input
+      className='form-control'
+      type="text"
+      placeholder='Enter your url'
+      value={url}
+      onChange={(e) => setUrl(e.target.value)}
+      />
+      <button className='btn btn-primary' onClick={fetchUrl}>Short Url</button>
+      </div>
+      <p className='text-center fw-bolder fs-4'>{shortedUrl}</p>
     </div>
   )
 }
